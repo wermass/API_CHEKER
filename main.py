2fc37b2f093148ff8a66ecfe7ea7eb3cf1f20794
dmitrijoresin <real432@rambler.ru>
2023-08-19 17:30:19 +0300

    Обновил согласно требованиям

--- main.py
+++ main.py
@@ -3,19 +3,41 @@ import requests
 import telebot
 
 API_URL = ''
-TOKEN = ''
-CHAT_ID = 0
+TELEGRAM_TOKEN = ''
+CHAT_ID_EVGEN = 149242583
+CHAT_ID_WERMASS = 5124962240
 
-bot = telebot.TeleBot(TOKEN)
+bot = telebot.TeleBot(TELEGRAM_TOKEN)
+
+SITE_IS_UP = True
+MESSAGE_SENT = False
 
 
 def check_api():
+    global SITE_IS_UP, MESSAGE_SENT
+
     try:
         response = requests.get(API_URL)
-        if response.status_code != 200:
-            raise Exception(f"API {response.status_code}: {response.text}")
-    except Exception as e:
-        bot.send_message(CHAT_ID, f"Ошибка API: {e}")
+        response.raise_for_status()
+
+        if not SITE_IS_UP and not MESSAGE_SENT:
+            bot.send_message(CHAT_ID_EVGEN, "Сайт ожил")
+            bot.send_message(CHAT_ID_WERMASS, "Сайт ожил")
+            SITE_IS_UP = True
+            MESSAGE_SENT = True
+
+        elif SITE_IS_UP and MESSAGE_SENT:
+            MESSAGE_SENT = False
+
+    except requests.RequestException:
+        if SITE_IS_UP or not MESSAGE_SENT:
+            bot.send_message(CHAT_ID_EVGEN, "Сайт упал")
+            bot.send_message(CHAT_ID_WERMASS, "Сайт упал")
+            SITE_IS_UP = False
+            MESSAGE_SENT = True
+
+        elif not SITE_IS_UP and MESSAGE_SENT:
+            MESSAGE_SENT = False
 
 
 if __name__ == '__main__':
